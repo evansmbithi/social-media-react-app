bab5ade1b5b3f2a4815d322200d3a6d387be85e0
evansmbithi <evholmbithy@gmail.com>
2025-05-05 10:28:52 +0300

    completed Profile Summary - LeftPane section

--- src/components/Body/LeftPane/ProfileSummary.js
+++ src/components/Body/LeftPane/ProfileSummary.js
@@ -1,11 +1,17 @@
 import "./../Body.css"
+import ProfilePic from "./../../../img/User-Profile-PNG-Image.png"
 
 function ProfileSummary(){
     return(
         <div className="profile-summary">
             <div className="cover-image">
-                <img alt="profile-photo"/>
+                <img src={ProfilePic} alt="profile-photo" className="profile-photo"/>
             </div>
+            <ul>
+                <li><button>TWEETS</button></li>
+                <li><button>FOLLOWING</button></li>
+                <li><button>FOLLOWERS</button></li>
+            </ul>
         </div>
     )
 }
